559bfba9bebf3fb4e53fbd82d594b30763a2ffb1
Cezary Sawicki <czarekpl5053@gmail.com>
2024-02-20 12:19:41 +0100

    GitIgnore

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,33 +5,34 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    
-    public class UsersController:ControllerBase
+
+    public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
         {
-            _context=context;
+            _context = context;
         }
-    
-    [HttpGet]
-    public ActionResult<IEnumerable<AppUsers>> GetUsers()
-    {
-        var users = _context.Users.ToList();
-        return users;
-    }
-    
-    [HttpGet("{id}")] // /api/users/2
-    public ActionResult<AppUsers>GetUser(int id)
-    {
-        var user = _context.Users.Find(id);
-        return user;
-    }
-    
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUsers>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")] // /api/users/2
+        public async Task<ActionResult<AppUsers>> GetUserAsync(int id)
+        {
+            var user = _context.Users.FindAsync(id);
+            return await user;
+        }
+
     }
 }
