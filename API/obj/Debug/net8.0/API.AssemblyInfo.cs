d3ea7f5c0cb3876216445a4269b43caf7121c269
Cezary Sawicki <czarekpl5053@gmail.com>
2024-02-28 23:19:43 +0100

    Account Register and home changes

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+999a6a5b4ba972dc7566c0d39f5b00fb7cd2aa19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0fbdb25d2d1d097472bcea50d58453dfc6b8546")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,19 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Dating App';
-  users: any;
 
 
-  constructor(private http: HttpClient) {
+
+  constructor( private accountService: AccountService) {
 
   }
   ngOnInit(): void {
-    this.http.get('http://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Requst has completed')
-    })
 
+    this.SetCurrentUser();
   }
 
+
+
+  SetCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user:User=JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
 }
