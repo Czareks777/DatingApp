60939c05cea92b02519429bc0388ae2714ebbbf9
Cezary Sawicki <czarekpl5053@gmail.com>
2024-03-24 22:18:53 +0100

    Client side notification about leaving page after editing

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cc3d94e54c42b2896ba4f827dcfb13bbc8c18ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+889a8991d5abdab8d62f6cdc653034cb2c24eec5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
