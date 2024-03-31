f9dd3f6b9c45ce1eb8a262bc986ea65dfd56858d
Cezary Sawicki <czarekpl5053@gmail.com>
2024-03-31 15:23:48 +0200

    Trying to understand pagination

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8e6d5f33eefeeb91d0a224c71e763b91b0ef27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00069b2c38985264f166114207801659cdcb74d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
